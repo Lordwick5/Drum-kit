571046e693ae29b98eab3dc750d2e22655e0bea4
Lordwick5 <Prashantchy96@gmail.com>
2025-04-04 22:48:45 +0545

    .

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrumButtons; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click",function () { 
-    alert("I got clicked!");
     
+        this.style.color = "white";
+
+
     });
 
 }
